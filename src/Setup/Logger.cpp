30551c272d75d85394d719cf230b9a72725bb1d5
Daniel Destouche <daniel.destouche@gmail.com>
2023-10-26 22:14:29 -0400

    fix: Cleanup unused code

--- src/Setup/Logger.cpp
+++ src/Setup/Logger.cpp
@@ -18,12 +18,10 @@ void CLogger::Log(wchar_t *path, const char *level, const wchar_t *logMsg)
 {
 	std::wstring ws(logMsg);
 	std::string strLogMsg(ws.begin(), ws.end());
-
+	
 	std::string now = getCurrentDateTime();
 	std::ofstream ofs(path, std::ios_base::out | std::ios_base::app);
 	ofs << now << '\t' << std::string(level) << '\t' << strLogMsg << '\n';
-	//ofs << now << '\t' << &level << '\t' << &logMsg << '\n';
-	//ofs << now << '\t' << *level << '\t' << *logMsg << '\n';
-	//ofs << now << '\t' << level << '\t' << *logMsg << '\n';
+
 	ofs.close();
 }
